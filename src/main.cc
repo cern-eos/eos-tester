ed25f861cad9bef84658d919c2340182ce423acf
Georgios Bitzes <georgios.bitzes@cern.ch>
2018-03-12 11:33:39 +0100

    Add eos-tester project skeleton

---
+++ src/main.cc
@@ -0,0 +1,33 @@
+// ----------------------------------------------------------------------
+// File: main.cc
+// Author: Georgios Bitzes - CERN
+// ----------------------------------------------------------------------
+
+/************************************************************************
+ * eos-tester - a tool for stress testing EOS instances                 *
+ * Copyright (C) 2016 CERN/Switzerland                                  *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#include <iostream>
+#include <sstream>
+
+int main(int argc, char **argv) {
+  std::ostringstream usage;
+  usage << "This tool collects a number of functional and stress tests for the EOS storage system." << std::endl;
+  usage << "Not safe to use against production instances!";
+
+  return 0;
+}
